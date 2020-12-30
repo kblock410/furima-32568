0f888c4aa90b0b9c62f8ad4b54825a81b9e11418
kblock410 <ken3090@icloud.com>
2020-12-30 16:45:38 +0900

    itemのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :explanation
+    validates :category_id
+    validates :state_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :days_to_delivery_id
+    validates :image
+
+    validates :price, format: { with:/\A[0-9]+\z/ }
+    validates :price, inclusion: { in: 300..9999999}
+  end
+
   belongs_to :user
   has_one :buy
   has_one_attached :image
