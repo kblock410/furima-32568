4d75ad6d945dca7da251ec691b149e7ea09d10f6
kblock410 <ken3090@icloud.com>
2021-01-05 19:32:09 +0900

    削除機能の修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,12 +23,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && current_user == @item.user
-      @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    @item.destroy if current_user == @item.user
+    redirect_to root_path
   end
 
 
