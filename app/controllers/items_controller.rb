db756bb6e5fcbdf51e88600099240e85befcd48b
kblock410 <ken3090@icloud.com>
2021-01-05 19:13:15 +0900

    before actionの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :destory]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user == @item.user
       @item.destroy
       redirect_to root_path
@@ -37,6 +36,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :explanation, :category_id, :state_id, :delivery_fee_id, :prefecture_id, :days_to_delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
